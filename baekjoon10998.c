3475b2a906a32adea3d79d012629f2d9ac391aa0
이호준2019313368 <hojoon1101@gmail.com>
2021-08-09 13:41:59 +0000

    baekjoon 10172

---
+++ baekjoon10998.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("\n%d\n",a*b);
+	
+	
+	
+	return 0;
+}
--- baekjoon_10172.c
+++ baekjoon_10172.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-	printf("|\\_/|\n");
-	printf("|q p|   /}\n");
-	printf("( 0 )\"\"\"\\\n");
-	printf("|\"^\"`    |\n");
+	printf("|\\_/| \n"); // \를 프린트 하려면 \\
+	printf("|q p|   /} \n");
+	printf("( 0 )\"\"\"\\ \n"); // "를 프린트 하려면\"
+	printf("|\"^\"`    | \n");
 	printf("||_/=\\\\__|");
 }
