29cd1aed8b941d0771a1298361e851d824568ea3
이호준2019313368 <hojoon1101@gmail.com>
2021-08-14 14:47:56 +0000

    baekjoon 1003

--- baekjoon_1003.c
+++ baekjoon_1003.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
+int arr[40];
 int fibonacci(int n) {
     if (n == 0) {
         return 0;
     } else if (n == 1) {
         return 1;
     } else {
-        return fibonacci(n-1) + fibonacci(n-2);
+        return arr[n-1] + arr[n-2];
     }
 }
 
+/*
 int main(void)
 {
 	int i;
@@ -23,5 +24,24 @@ int main(void)
 			printf("1 0\n");
 		else
 			printf("%d %d\n",fibonacci(array[i]-1),fibonacci(array[i]));
+	}
+}
+*/
+
+int main(void)
+{
+	int i, index,number;
+	for(i=0;i<=40;i++)
+	{
+		arr[i]=fibonacci(i);
+	}
+	scanf("%d",&number);
+	for(i=0;i<number;i++)
+	{
+		scanf("%d",&index);
+		if(index==0)
+			printf("1 0\n");
+		else
+			printf("%d %d\n",arr[index-1], arr[index]);
 	}
 }
