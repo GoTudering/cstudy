68025113f9b5c941bb34b19098ad6978cd120a7e
이호준2019313368 <hojoon1101@gmail.com>
2021-08-21 05:32:16 +0000

    baekjoon 1110

--- baekjoon_10951.c
+++ baekjoon_10951.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	int a,b;
-	while(scanf("%d %d",&a,&b)!=EOF) //EOF는 End Of File로 아무 입력이 없음을 의미한다.
+	while(scanf("%d %d",&a,&b)!=EOF) //EOF는 End Of File로 아무 입력이 없음을 의미한다. ctrl+z를 입력하면 EOF로 받아드린다.
 	{
 		printf("%d\n",a+b);
 	}
--- baekjoon_1110.c
+++ baekjoon_1110.c
@@ -2,7 +2,18 @@
 
 int main(void)
 {
-	int a,b,c,count;
-	scanf("%d",a);
-	
+	int a,b,c,d,count;
+	count=1;
+	scanf("%d",&a);
+	b=a/10;
+	c=a%10;
+	d=c*10+(b+c)%10;
+	while(a!=d)
+	{
+		b=c;
+		c=d%10;
+		d=c*10+(b+c)%10;
+		count+=1;
+	}
+	printf("%d",count);
 }
