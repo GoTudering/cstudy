482f228303d78c9e1c4993a63eeccafb9f2c6562
이호준2019313368 <hojoon1101@gmail.com>
2021-08-25 14:47:14 +0000

    baekjoon 2941

--- baekjoon_2941.c
+++ baekjoon_2941.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
+int main(void)
+{
+	char word[101];
+	scanf("%s",word);
+	int count=0;
+	for(int i=0;i<strlen(word);i++)
+	{
+		if(word[i]==*"-"||word[i]==*"=")
+			continue;
+		else if(word[i]==*"d"&&word[i+1]==*"z"&&word[i+2]==*"="&&i+2<strlen(word))
+		{
+			count++;
+			i+=2;
+		}
+		else if(word[i]==*"l"&&word[i+1]==*"j"&&i+1<strlen(word))
+		{
+			count++;
+			i+=1;
+		}
+		else if(word[i]==*"n"&&word[i+1]==*"j"&&i+1<strlen(word))
+		{
+			count++;
+			i+=1;
+		}
+		else
+			count++;
+	}
+	printf("%d",count);
+}
