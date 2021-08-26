97839ea609c9545470a7096a9f1bc15066266e00
이호준2019313368 <hojoon1101@gmail.com>
2021-08-26 13:47:30 +0000

    baekjoon 1316

--- baekjoon_1316.c
+++ baekjoon_1316.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-int equal(int i, char *a)
+int equal(int i, char a[])
 {
 	if(a[i]==a[i+1])
+	{
 		i++;
+		return equal(i,a);
+	}
 	else
 		return i;
 }
@@ -14,14 +17,25 @@ int main(void)
 	scanf("%d",&N);
 	for(int i=0;i<N;i++)
 	{
-		char arr[100];
+		char arr[101]={'\0',}, check[26]={'\0',};
+		int chk=1;
 		scanf("%s",arr);
-		char *p=arr;
 		for(int j=0;arr[j]!=*"\0";j++)
 		{
-			equal(i,p);
-			count++;
+			if(check[(int)arr[j]-97]!='O')
+			{
+				check[(int)arr[j]-97]='O';
+			}
+			else
+			{
+				chk=0;
+				break;
+			}
+			j=equal(j,arr);
 		}
+		if(chk==1)
+			count++;
+		
 	}
 	printf("%d",count);
 }
