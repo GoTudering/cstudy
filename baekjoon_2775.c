0500afe9cc759aff5b491af9b021093d07023ddc
이호준2019313368 <hojoon1101@gmail.com>
2021-08-27 14:46:06 +0000

    baekjoon 2775

---
+++ baekjoon_2775.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[15][15]={0,};
+	
+	for(int i=0;i<15;i++)
+		arr[0][i]=i;
+	for(int i=1;i<15;i++)
+		for(int j=1;j<15;j++)
+			arr[i][j]=arr[i-1][j]+arr[i][j-1];
+	
+	int T;
+	scanf("%d",&T);
+	for(int i=0;i<T;i++)
+	{
+		int k,n;
+		scanf("%d %d",&k,&n);
+		printf("%d\n",arr[k][n]);
+	}
+}
