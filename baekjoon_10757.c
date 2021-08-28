32f262b23f0ef3828cea042cef070bd9e3bf5029
이호준2019313368 <hojoon1101@gmail.com>
2021-08-28 14:17:17 +0000

    baekjoon 10757

---
+++ baekjoon_10757.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char arr[])
+{
+	int len=strlen(arr);
+	char a[len];
+	for(int i=0;i<len;i++)
+	{
+		a[i]=arr[len-1-i];
+	}
+	for(int i=0;i<len;i++)
+	{
+		arr[i]=a[i];
+	}
+}
+
+int main(void)
+{
+	char A[10002],B[10002],retn[10003];
+	int next=0,max;
+	scanf("%s %s",A,B);
+	reverse(A);
+	reverse(B);
+	max=(strlen(A)>strlen(B)) ? strlen(A):strlen(B);
+	for(int i=0;i<=max;i++)
+	{
+		int sum;
+		sum=A[i]-'0'+B[i]-'0'+next;
+		while(sum<0)
+			sum+='0';
+		if(sum>9)
+			next=1;
+		else
+			next=0;
+		retn[i]=sum%10+'0'	;
+	}
+	reverse(retn);
+	printf("%s",retn);
+}
